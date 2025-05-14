2dd16a71802987910f7f89861cbcad4a8f15cbf3
José Cestaro <zertexlion@gmail.com>
2025-05-14 15:41:14 -0300

    configurando corretamente o docker e ajustando projeto para subir

--- src/main/java/com/github/jcestaro/url_shortener/UrlShortenerApplication.java
+++ src/main/java/com/github/jcestaro/url_shortener/UrlShortenerApplication.java
@@ -2,12 +2,17 @@ package com.github.jcestaro.url_shortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 public class UrlShortenerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UrlShortenerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShortenerApplication.class, args);
+    }
 
 }
--- src/main/java/com/github/jcestaro/url_shortener/infra/UrlMappingRepository.java
+++ src/main/java/com/github/jcestaro/url_shortener/infra/UrlMappingRepository.java
@@ -2,10 +2,12 @@ package com.github.jcestaro.url_shortener.infra;
 
 import com.github.jcestaro.url_shortener.model.UrlMapping;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UrlMappingRepository extends MongoRepository<UrlMapping, UUID> {
 
     Optional<UrlMapping> findByShortCode(String shortCode);
